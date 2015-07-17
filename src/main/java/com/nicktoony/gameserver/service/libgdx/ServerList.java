d4a367556ef839bcfdb0798d699dcb1cde1543ac
NickToony <nicktoony@gmail.com>
2015-07-17 17:15:19 +0100

    Version to work with space battle game.

--- src/main/java/com/nicktoony/gameserver/service/libgdx/ServerList.java
+++ src/main/java/com/nicktoony/gameserver/service/libgdx/ServerList.java
@@ -12,9 +12,9 @@ import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
-import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
 import com.nicktoony.gameserver.service.client.Client;
 import com.nicktoony.gameserver.service.client.models.Server;
 
